f5f793b7a011c0ef438a810615d75c1c71af413f
Gerwick <egerwick@yahoo.com>
2016-10-12 10:54:43 +0200

    bonjour py

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import trueskill
+import bonjour-py
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
