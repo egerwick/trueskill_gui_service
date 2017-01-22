b1d8bd07bea3628ab8ce5bcf4f78dbb0463c1038
Gerwick <egerwick@yahoo.com>
2017-01-22 12:59:16 +0100

    with db model

--- hello/models.py
+++ hello/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Greeting(models.Model):
+    when = models.DateTimeField('date created', auto_now_add=True)
+
 class Player(models.Model):
     id = models.IntegerField(primary_key=True)
     nickname = models.CharField(max_length=100, blank=True, default='')
--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from models import Game, GamePerformance, Player
+from models import Game, GamePerformance, Player, Greeting
 from serializers import GameSerializer, GamePerformanceSerializer, PlayerSerializer
 from django.utils.six import BytesIO
 from trueskill.one_game_update import get_rating
