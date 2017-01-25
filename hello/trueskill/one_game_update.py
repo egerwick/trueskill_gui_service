21e1f81bb9475989fce7424579c2b78b633a95dc
Gerwick <egerwick@yahoo.com>
2017-01-25 09:48:57 +0100

    added dynamical factor

--- hello/trueskill/one_game_update.py
+++ hello/trueskill/one_game_update.py
@@ -5,7 +5,7 @@ def get_rating():
     games = extract()
     games.reverse()
     f_mu = 25.0
-    f_tau = 0.083333
+    f_tau = 2*0.083333
     track_mu_over_time = False
     list_players = games_in_trueskill(games, f_mu, f_tau, track_mu_over_time, 0	)
     min_number_games = 20
