2b5a67c50a26920b8a2caa8d5bf17c70d331b22f
Gerwick <egerwick@yahoo.com>
2017-01-26 14:21:03 +0100

    bug fix

--- hello/trueskill/trueskill_lib.py
+++ hello/trueskill/trueskill_lib.py
@@ -201,6 +201,6 @@ def player_ranking_to_json(f_list_players, f_min_number_games, last_month = Fals
     for p in f_list_players:
         total_games = total_games + float(p.ngame)
         if(p.ngame > f_min_number_games):
-            p_tmp = {"name": p.name,"position": p.position ,"skill": p.rating.skill,"mu": p.rating.mu, "sigma": p.rating.sigma, "winnerPercentage": float(p.won) / float(p.ngame), "goalAverage":float(p.goals) / float(p.ngame)}
+            p_tmp = {"name": p.name,"position": p.position ,"skill": p.skill,"mu": p.rating.mu, "sigma": p.rating.sigma, "winnerPercentage": float(p.won) / float(p.ngame), "goalAverage":float(p.goals) / float(p.ngame)}
             tmp_data.append(p_tmp)
     return tmp_data
